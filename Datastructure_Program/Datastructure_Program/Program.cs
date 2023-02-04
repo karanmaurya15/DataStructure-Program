75a40cb45f6cba27cefec65256c8efc54645e227
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-04 15:18:07 +0530

    [Karan] Ordered List

--- Datastructure_Program/Datastructure_Program/Program.cs
+++ Datastructure_Program/Datastructure_Program/Program.cs
@@ -5,13 +5,14 @@ namespace Datastructure_Program
     internal class Program
     {
         public static string wordFilePath = @"C:\BridgeLabz\DataStructure-Program\Datastructure_Program\Datastructure_Program\Files\WordSearch.txt";
+        public static string numberFilePath = @"C:\BridgeLabz\DataStructure-Program\Datastructure_Program\Datastructure_Program\Files\NumberFile.txt";
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to the Data Structure Programs");
             bool flag = true;
             while(flag)
             {
-                Console.WriteLine(" 1.UnOrdered List \n 2.Exit");
+                Console.WriteLine(" 1.UnOrdered List \n 2.Ordered List \n 3.Exit");
                 Console.Write("Select any one from the above option :");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
@@ -21,6 +22,10 @@ namespace Datastructure_Program
                         unOrderedList.ReadAllText(wordFilePath);
                         break;
                     case 2:
+                        OrderedList orderedList = new OrderedList();
+                        orderedList.ReadAllTextFile(numberFilePath);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
---
+++ Datastructure_Program/Datastructure_Program/Utility/OrderedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure_Program.Utility
+{
+    internal class OrderedList
+    {
+        public void ReadAllTextFile(string filepath)
+        {
+            string list = File.ReadAllText(filepath);
+            string[] numbers = list.Split(",");
+            SortingList<int> linkedlist = new SortingList<int>();
+            foreach (var num in numbers)
+            {
+                int result = Convert.ToInt32(num);
+                linkedlist.AddNode(result);
+            }
+            Console.WriteLine("Before Sorting");
+            linkedlist.DisplaySort();
+            linkedlist.SortOrderedList();
+            Console.WriteLine("After Sorting : ");
+            linkedlist.DisplaySort();
+        }
+    }
+}
